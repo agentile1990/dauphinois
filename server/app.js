140eb83fc457f1153a6a80684754ac63f7152016
Andrew Gentile <agentile1990@gmail.com>
2020-08-30 23:00:36 -0500

    Handle Async Express Errors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 require('./db');
-const createError = require('http-errors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -14,16 +13,11 @@ app.use(cookieParser());
 
 app.use('/recipes', require('./apps/recipes/recipes.app'));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
-
 // error handler
-app.use(function (err, req, res) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+// next() required for wrap()
+// eslint-disable-next-line no-unused-vars
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
 
     res.status(500).send({ message: 'Internal Server Error' });
 });
--- server/apps/recipes/recipes.routes.js
+++ server/apps/recipes/recipes.routes.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const { wrap } = require('@awaitjs/express');
 const router = express.Router();
 
 const Recipes = require('./recipes.controller');
 
-router.route('/').get(Recipes.list).post(Recipes.create);
+router.route('/').get(wrap(Recipes.list)).post(wrap(Recipes.create));
 
 router
     .route('/:id')
-    .get(Recipes.find)
-    .put(Recipes.update)
-    .delete(Recipes.delete);
+    .get(wrap(Recipes.find))
+    .put(wrap(Recipes.update))
+    .delete(wrap(Recipes.delete));
 
 module.exports = router;
