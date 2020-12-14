487e0114f395aba7eb5a8b519a93a589df82325f
Andrew Gentile <agentile1990@gmail.com>
2020-12-13 20:29:08 -0600

    Regex on Mongo DB Id

--- server/apps/recipes/recipes.routes.js
+++ server/apps/recipes/recipes.routes.js
@@ -7,7 +7,7 @@ const Recipes = require('./recipes.controller');
 router.route('/').get(wrap(Recipes.list)).post(wrap(Recipes.create));
 
 router
-    .route('/:id')
+    .route('/:id([a-fA-F0-9-]{24})')
     .get(wrap(Recipes.find))
     .put(wrap(Recipes.update))
     .delete(wrap(Recipes.delete));
